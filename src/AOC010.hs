89733823d9a13d79805c6d058a9fafd7830ad1a1
Will Song <incertia@incertia.net>
2020-12-10 01:58:12 -0600

    fix bugs

--- src/AOC010.hs
+++ src/AOC010.hs
@@ -14,14 +14,14 @@ import Control.Lens
 import Data.Function
   ((&))
 import Data.List
-  (sort, maximumBy, groupBy)
+  (sort)
 import Data.Vector
   (Vector, fromList, (!))
 import Problem
   (Solved(..), Part(..))
 
 import qualified Data.Vector as V
-  (head, tail, cons, filter, length)
+  (tail, cons, filter, length)
 
 jd :: Integer -> Vector Integer -> Integer
 jd n v =
@@ -34,32 +34,36 @@ jolts inp = let x = fmap read . lines $ inp in fromList . sort $ 0 : maximum x +
 
 groupjolts :: Vector Integer -> Vector (Vector Integer)
 groupjolts = V.filter (not . null)
-           . fmap ((\v -> let m = V.head v in (`subtract` m) <$> V.tail v) . fmap fst)
+           . fmap ((\v -> subtract (v ! 0) <$> V.tail v) . fmap fst)
            . vgroup snd . findgroups 0
-  where findgroups i v =
-          if V.length v >= 2 then
-            let (x, y) = (v ! 0, v ! 1)
-                t = V.tail v
-                rest = if y - x == 3 then V.cons (y, i) (findgroups (i + 1) t)
-                       else findgroups i t
-            in  V.cons (x, i) rest
-          else (,i) <$> v
-        vgroup f v =
-          if null v then []
-          else if V.length v == 1 then [v]
-          else
-            let r = vgroup f v
-            in  if f (r ! 0 ! 0) == f (v ! 0) then
-                   r & ix 0 %~ V.cons (v ! 0)
-                else V.cons [v ! 0] r
+
+vgroup :: Eq b => (a -> b) -> Vector a -> Vector (Vector a)
+vgroup f v =
+  if null v then []
+  else if V.length v == 1 then [v]
+  else
+    let r = vgroup f (V.tail v)
+    in  if f (r ! 0 ! 0) == f (v ! 0) then
+           r & ix 0 %~ V.cons (v ! 0)
+        else V.cons [v ! 0] r
+
+findgroups :: Integer -> Vector Integer -> Vector (Integer, Integer)
+findgroups i v =
+  if V.length v >= 2 then
+    let (x, y) = (v ! 0, v ! 1)
+        t = V.tail v
+        rest = if y - x == 3 then V.cons (y, i) (findgroups (i + 1) t)
+               else findgroups i t
+    in  V.cons (x, i) rest
+  else (,i) <$> v
 
 ways :: Vector Integer -> Integer
-ways v = go 0 v where
+ways v' = go 0 v' where
   go j v =
     if null v then if j == m then 1 else 0
     else if v ! 0 > j + 3 then 0
     else go (v ! 0) (V.tail v) + go j (V.tail v)
-  m = maximum v
+  m = maximum v'
 
 instance Solved 10 'PartA where
   solve = show . uncurry (*) . (jd 1 &&& jd 3) . jolts
